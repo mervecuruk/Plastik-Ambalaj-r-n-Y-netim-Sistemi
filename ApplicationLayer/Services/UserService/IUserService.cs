02da381908c071fd1a314dd97ee63fb575d6c9c5
mervecuruk <mervecuruk06@gmail.com>
2024-09-18 23:47:02 +0300

    userdetail metotları oluşturuldu

--- ApplicationLayer/Services/UserService/IUserService.cs
+++ ApplicationLayer/Services/UserService/IUserService.cs
@@ -4,6 +4,9 @@ namespace ApplicationLayer.Services.UserService
 {
     public interface IUserService
     {
+        Task<List<UserDetailDTO>> GetAllUsersDetailAsync();
+        Task<UserDetailDTO> GetUserDetailAsync(int userId);
         Task<bool> RegisterAsync(CreateUserDTO user);
+        Task<bool> UpdateAdminAsync(int userId, UpdateUserDTO updateUserDTO);
     }
 }
